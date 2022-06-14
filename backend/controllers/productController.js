4d80f883037a4c9db14c429bc548591995d0968b
Kabir3008 <baktiar.kabir.joy3008.com>
2022-06-14 17:07:28 +0600

    product adding

---
+++ backend/controllers/productController.js
@@ -0,0 +1,19 @@
+const Product = require('../models/product')
+
+//Create new product => /api/v1/product/next
+
+exports.newProduct= async(req,res,next) => {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })  
+}
+
+exports.getProducts = (req, res, next) =>{
+    res.status(200).json({
+        sucess: true,
+        message: "This route will show all products in database."
+    })
+}
