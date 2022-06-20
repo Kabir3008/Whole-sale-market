18cceff1968e52fa675512519d519a6d57dd7e72
Kabir3008 <baktiar.kabir.joy3008.com>
2022-06-20 12:26:55 +0600

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,12 +19,18 @@ exports.newProduct= catchAsyncErrors (async(req,res,next) => {
 
 //get all products => /api/v1/products
 exports.getProducts = catchAsyncErrors (async(req, res, next) =>{
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search()
+    
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments()
+    const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter().pagination(resPerPage)
+    
     const products = await apiFeatures.query;
+    
     res.status(200).json({
         
         sucess: true,
         count:products.length,
+        productCount,
         products
     })
 })
