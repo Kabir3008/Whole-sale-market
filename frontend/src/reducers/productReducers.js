c6ddd0f9806e6db655e3e81356167092a8418d4f
Kabir3008 <baktiar.kabir.joy3008.com>
2022-08-12 16:18:47 +0600

    review

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -20,7 +20,7 @@ export const productsReducer = (state = { products: [] }, action) => {
             return {
                 loading: false,
                 products: action.payload.products,
-                productsCount: action.payload.productsCount
+                productCount: action.payload.productCount
 
             }
 
