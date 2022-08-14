67c54a91ce9521c5ca3b83d681e46d11b4f689b2
Kabir3008 <baktiar.kabir.joy3008.com>
2022-08-14 15:33:32 +0600

    filter by price

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,8 @@
 // import React, { useEffect } from 'react'
 import React, { Fragment, useState, useEffect } from 'react'
 import Pagination from 'react-js-pagination'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css';
 import MetaData from './layout/MetaData'
 
 import { useDispatch, useSelector } from 'react-redux'
@@ -11,8 +13,19 @@ import Loader from './layout/Loader'
 import { useAlert } from 'react-alert'
 import { useParams } from "react-router-dom";
 
+// const { createSliderWithTooltip } = Slider;
+// const Range = createSliderWithTooltip(Slider.Range);
+
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
+
+
+
 const Home = () => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [price, setPrice] = useState([1, 1000])
     const alert = useAlert();
     const dispatch = useDispatch();
     const { loading, products, error, productCount, resPerPage } = useSelector(state => state.products)
@@ -24,8 +37,8 @@ const Home = () => {
             // alert.success('Success')
             return alert.error(error)
         }
-        dispatch(getProducts(keyword, currentPage));
-    }, [dispatch, alert, error, keyword, currentPage])
+        dispatch(getProducts(keyword, currentPage, price));
+    }, [dispatch, alert, error, keyword, price, currentPage])
 
 
     function setCurrentPageNo(pageNumber) {
@@ -42,9 +55,45 @@ const Home = () => {
                     <h1 id="products_heading">Latest Products</h1>
                     <section id="products" className="container mt-5">
                         <div className="row">
-                            {products && products.map(product => (
-                                <Product key={product._id} product={product} />
-                            ))}
+                            {keyword ? (
+                                <Fragment>
+                                    <div className="col-6 col-md-3 mt-5 mb-5">
+                                        <div className="px-5">
+                                            <Slider.Range
+                                                marks={{
+                                                    1: `$1`,
+                                                    1000: `$1000`
+                                                }}
+                                                min={1}
+                                                max={1000}
+                                                defaultValue={[1, 1000]}
+                                                tipFormatter={value => `$${value}`}
+                                                tipProps={{
+                                                    placement: "top",
+                                                    visible: true
+                                                }}
+                                                value={price}
+                                                onChange={price => setPrice(price)}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-6 col-md-9">
+                                        <div className="row">
+                                            {products.map(product => (
+                                                <Product key={product._id} product={product} col={4} />
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                </Fragment>
+
+                            ) : (products && products.map(product => (
+                                <Product key={product._id} product={product} col={3} />
+                            ))
+
+                            )}
+
 
                         </div>
                     </section >
@@ -66,7 +115,8 @@ const Home = () => {
                         </div>
                     )}
                 </Fragment>
-            )}
+            )
+            }
 
 
         </Fragment >
