b9f39b1831f5f6931522a084a77cc4c6bc724e1c
Kabir3008 <baktiar.kabir.joy3008.com>
2022-08-16 20:47:58 +0600

    password reset

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, getUserProfile, updateProfile, updatePassword, logout, allUsers, forgotPassword } = require('../controllers/authController');
+const { registerUser, loginUser, getUserProfile, updateProfile, updatePassword, resetPassword, logout, allUsers, forgotPassword } = require('../controllers/authController');
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
@@ -9,6 +9,7 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(logout);
 router.route('/me').get(isAuthenticatedUser, getUserProfile); //its a protected route.cz user need to login first to see his profile. 
 router.route('/password/update').put(isAuthenticatedUser, updatePassword); //user password update korte chaile
