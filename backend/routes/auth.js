62063cdff653ee069df0964f4015814b5cc1df09
Kabir3008 <baktiar.kabir.joy3008.com>
2022-08-16 20:35:25 +0600

    password recovery

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, getUserProfile, updateProfile, updatePassword, logout, allUsers } = require('../controllers/authController');
+const { registerUser, loginUser, getUserProfile, updateProfile, updatePassword, logout, allUsers, forgotPassword } = require('../controllers/authController');
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/password/forgot').post(forgotPassword);
 router.route('/logout').get(logout);
 router.route('/me').get(isAuthenticatedUser, getUserProfile); //its a protected route.cz user need to login first to see his profile. 
 router.route('/password/update').put(isAuthenticatedUser, updatePassword); //user password update korte chaile
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);//user profile update korte chaile
+
 
 router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers); //only for admin to access users
 
